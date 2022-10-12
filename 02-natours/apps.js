7cd83454efcfc8183d1123d08e479baa46d0c391
suryapideyaLabs <suryap@ideyalabs.com>
2022-10-12 16:50:56 +0530

    natour get and post methods

--- 02-natours/apps.js
+++ 02-natours/apps.js
@@ -1,7 +1,68 @@
 import express from 'express';
-
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+// create an app
 let app = express();
+
+// add a middleware to read the data from the requests
+app.use(express.json());
+
 let port = 3000;
+let __filename = fileURLToPath(import.meta.url);
+let __dirname = path.dirname(__filename);
+let tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+// get all the tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// create a new tours
+app.post('/api/v1/tours', (req, res) => {
+  let newID = tours[tours.length - 1].id + 1;
+  let newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// get tour details by ID
+app.get('/api/v1/tours/:id', (req, res) => {
+  let _id = req.params.id * 1;
+  let tour = tours.find((tr) => tr.id === _id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.listen(port, () => {
   console.log('App is running on port ', port);
 });
