586bb7b556dc2da3cd44a8652ffe15c660d27e51
suryapideyaLabs <suryap@ideyalabs.com>
2022-10-19 15:32:19 +0530

    created first schema and model for tours

--- 02-natours/server.js
+++ 02-natours/server.js
@@ -8,7 +8,27 @@ const port = process.env.PORT || 3000;
 
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
-mongoose.connect(DB).then((con) => console.log(con));
+mongoose.connect(DB).then(() => console.log('Connected DB Successfully...'));
+
+// create a tour schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour price is required'],
+  },
+});
+
+// create a model from the schema
+const Tour = mongoose.model('Tour', tourSchema);
 
 app.listen(port, () => {
   console.log('App is running on port ', port);
